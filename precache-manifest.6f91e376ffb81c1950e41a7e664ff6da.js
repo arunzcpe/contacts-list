390a8cc45e2f919ea115c39da7004a819b9f118c
arun <aarunit@gmail.com>
2019-11-22 18:16:54 +0800

    Updates

---
+++ precache-manifest.6f91e376ffb81c1950e41a7e664ff6da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5bf581eb9a5c34b03c8973bdfcd8dfd",
+    "url": "arunzcpe.github.io/contacts-list/index.html"
+  },
+  {
+    "revision": "1e60a0a9e498bc047bdf",
+    "url": "arunzcpe.github.io/contacts-list/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6b152233a756dc8bdaf6",
+    "url": "arunzcpe.github.io/contacts-list/static/js/2.5164fc0b.chunk.js"
+  },
+  {
+    "revision": "1e60a0a9e498bc047bdf",
+    "url": "arunzcpe.github.io/contacts-list/static/js/main.30b10734.chunk.js"
+  },
+  {
+    "revision": "89c81a4b6f869b8a51c8",
+    "url": "arunzcpe.github.io/contacts-list/static/js/runtime-main.b3754d3f.js"
+  }
+]);
